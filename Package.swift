7f5a28e05e61ffad33dfd36745819d9b345d5f22
Phanidhar <phanidhar.gudi@gmail.com>
2020-08-11 13:14:13 -0700

    Danger Added

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+  name: "DangerSwift",
+  products: [
+    // Library that contains all of the dependencies.
+    .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"])
+
+  ],
+  dependencies: [
+    // Danger.
+    .package(url: "https://github.com/danger/swift.git", from: "3.3.1"),
+
+    // Danger Plugins.
+    .package(url: "https://github.com/f-meloni/danger-swift-xcodesummary.git", from: "1.2.1"),
+    .package(url: "https://github.com/f-meloni/danger-swift-coverage.git", from: "1.1.0"),
+    .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.3.0"),
+
+  ],
+  targets: [
+    .target(name: "DangerDependencies",
+            dependencies: ["Danger", "DangerPlugins", "DangerSwiftCoverage", "DangerXCodeSummary", "ShellOut"],
+            path: "DangerDependencies")
+  ]
+)
